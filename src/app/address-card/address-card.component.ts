7ef5dcf403e5b9f0d2e7a98ac1c2d0b1f2bc4ba1
Troy.Edward.Lee <39561789+TroyEdwardLee@users.noreply.github.com>
2018-09-09 22:56:37 +0800

    [FEATURE] pase parameter to component

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-address-card',
@@ -7,19 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddressCardComponent implements OnInit {
   user: any;
+  @Input('userName') userName: string;
+  @Input('componentTitle') componentTitle: string;
   constructor() {
+  }
+
+  ngOnInit() {
     this.user = {
-      name: 'Foo Bar',
-      title: 'Software Developer',
+      name: this.userName,
+      title: this.componentTitle,
       address: '1234 Main St, City, State, 10010',
       phone: [
         '123-123-1234',
         '456-456-4568'
       ]
     };
-  }
-
-  ngOnInit() {
   }
 
 }
